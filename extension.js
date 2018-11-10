e3be89efe476cdec82a20afb9067146fc843a371
Nate Parrish <nate.parrish@arrowpointe.com>
2018-11-09 16:58:17 -0800

    Add diagnositcs when on save deploys fail

--- extension.js
+++ extension.js
@@ -2,12 +2,13 @@ const vscode = require('vscode');
 const spawn = require('child_process').spawn;
 const path = require('path');
 const _ = require('lodash');
-const Uri = require('vscode-uri').default;
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 
 function activate(context) {
     const output = vscode.window.createOutputChannel('NateMate');
+
+    const diagnosticCollection = vscode.languages.createDiagnosticCollection('sfdx');
 
     function exec(cmd, options) {
         const cmdSplit = cmd.split(' ');
@@ -24,11 +25,13 @@ function activate(context) {
             });
     
             stream.stderr.on('data', (data) => {
+                result += data.toString();
+
                 output.appendLine(data.toString());
             });
     
             stream.on('exit', (code) => {
-                if (code) reject(new Error('exit code ' + code.toString()));
+                if (code) reject(result);
     
                 resolve(result);
             });
@@ -42,16 +45,28 @@ function activate(context) {
 
         output.appendLine(`deploying: ${relativeFilePath}`);
 
+        diagnosticCollection.clear();
+
         try {
             await vscode.window.withProgress({
                 location: vscode.ProgressLocation.Notification,
                 title: `deploying: ${relativeFilePath}`,
                 cancellable: false
-            }, exec.bind(null, `sfdx force:source:deploy -p ${relativeFilePath}`, {cwd: workspacePath}));
+            }, exec.bind(null, `sfdx force:source:deploy --json -p ${relativeFilePath}`, {cwd: workspacePath}));
 
             vscode.window.showInformationMessage(`deployed: ${relativeFilePath}`);
         } catch (err) {
-            output.appendLine(err);
+            const errorMessage = JSON.parse(err);
+
+            const problems = errorMessage.result;
+
+            const diagnostics = problems.map((problem) => {
+                const range = new vscode.Range(Number(problem.lineNumber) - 1, Number(problem.columnNumber) - 1, Number(problem.lineNumber) - 1, Number(problem.columnNumber) - 1);
+
+                return new vscode.Diagnostic(range, problem.error.replace(/ {1}\([0-9]+:[0-9]+\)$/, ''));
+            });
+
+            diagnosticCollection.set(vscode.Uri.file(path.join(workspacePath, relativeFilePath)), diagnostics);
 
             vscode.window.showErrorMessage(`failed to deploy: ${relativeFilePath}`);
         }
@@ -82,9 +97,9 @@ function activate(context) {
 
     context.subscriptions.push(vscode.workspace.onDidSaveTextDocument(async (filePath) => {
         try {
-            const workspacePath = vscode.workspace.getWorkspaceFolder(Uri.file(filePath.fileName)).uri.fsPath;
+            const workspacePath = vscode.workspace.getWorkspaceFolder(vscode.Uri.file(filePath.fileName)).uri.fsPath;
 
-            const relativeFilePath = `.${filePath.fileName.replace(workspacePath, '')}`;
+            const relativeFilePath = path.join('.', filePath.fileName.replace(workspacePath, ''));
             
             const sfRegex = /\.page$|\.component$|\.cls$|\.object$|\.trigger$|\.layout$|\.resource$|\.remoteSite$|\.labels$|\.app$|\.dashboard$|\.permissionset$|\.workflow$|\.email$|\.profile$|\.scf$|\.queue$|\.reportType$|\.report$|\.weblink$|\.tab$|\.letter$|\.role$|\.homePageComponent$|\.homePageLayout$|\.objectTranslation$|\.flow$|\.datacategorygroup$|\.snapshot$|\.site$|\.sharingRules$|\.settings$|\.callCenter$|\.community$|\.authProvider$|\.customApplicationComponent$|\.quickAction$|\.approvalProcess$|\.apxc$|\.apxt$/;
 
@@ -99,7 +114,7 @@ function activate(context) {
             } else if (resourceRegexMatch) {
                 await exec(`zip -FSr ${path.join(workspacePath, 'src', 'staticresources', resourceRegexMatch[0])} .`, {cwd: path.join(workspacePath, 'resource-bundles', resourceRegexMatch[0]), console: false});
     
-                addDeployFile(`./${path.join('src', 'staticresources', resourceRegexMatch[0])}`, workspacePath);
+                addDeployFile(path.join('src', 'staticresources', resourceRegexMatch[0]), workspacePath);
             }
         } catch (err) {
             output.appendLine(err);
@@ -107,7 +122,7 @@ function activate(context) {
     }));
 
     async function compileProject(workspaceFolder) {
-        const files = await fs.readdirAsync(`${workspaceFolder.uri.fsPath}/resource-bundles`);
+        const files = await fs.readdirAsync(path.join(workspaceFolder.uri.fsPath, 'resource-bundles'));
 
         const resourceBundles = [];
 
@@ -121,9 +136,9 @@ function activate(context) {
 
         await exec('rm -rf force-app', {cwd: workspaceFolder.uri.fsPath});
 
-        await exec('sfdx force:mdapi:convert --rootdir src/', {cwd: workspaceFolder.uri.fsPath, console: false});
+        await exec(`sfdx force:mdapi:convert --rootdir ${path.join('src')}`, {cwd: workspaceFolder.uri.fsPath, console: false});
 
-        return exec('sfdx force:source:deploy -p force-app/', {cwd: workspaceFolder.uri.fsPath});
+        return exec(`sfdx force:source:deploy -p ${path.join('force-app')}`, {cwd: workspaceFolder.uri.fsPath});
     }
 
     context.subscriptions.push(vscode.commands.registerCommand('extension.compileProject', async () => {
